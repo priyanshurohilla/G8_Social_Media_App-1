5e75065743dde283eb0b7a03d50f2c015455d70c
Rohit Suryanarayan <53330475+rohitsuryanarayan@users.noreply.github.com>
2022-07-26 16:33:02 +0530

    added User Entity

--- backend-socialmedia-final/src/main/java/app/socialmedia/model/User.java
+++ backend-socialmedia-final/src/main/java/app/socialmedia/model/User.java
@@ -1,4 +1,79 @@
 package app.socialmedia.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    private int userId;
+
+    private String fullName;
+
+    private String userName;
+
+    private String email;
+
+    private String profilePicUrl;
+
+    private String phoneNumber;
+
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePicUrl() {
+        return profilePicUrl;
+    }
+
+    public void setProfilePicUrl(String profilePicUrl) {
+        this.profilePicUrl = profilePicUrl;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
