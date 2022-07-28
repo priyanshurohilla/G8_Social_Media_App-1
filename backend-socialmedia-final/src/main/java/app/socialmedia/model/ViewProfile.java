300466d4a7060622362bb6b0531d8db57394adee
biswaranjanbehera <bmuna314@gmail.com>
2022-07-28 17:00:49 +0530

    View Profile Added

---
+++ backend-socialmedia-final/src/main/java/app/socialmedia/model/ViewProfile.java
@@ -0,0 +1,62 @@
+package app.socialmedia.model;
+
+import org.springframework.data.annotation.Id;
+
+public class ViewProfile {
+    @Id
+    private String fullName;
+
+    private String userName;
+    private String email;
+    private String phoneNumber;
+    private int following;
+    private int follower;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(int following) {
+        this.following = following;
+    }
+
+    public int getFollower() {
+        return follower;
+    }
+
+    public void setFollower(int follower) {
+        this.follower = follower;
+    }
+}
