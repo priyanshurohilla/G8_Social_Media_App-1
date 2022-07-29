6b327378ab1331ce2b2f6ff23e039581938d7b61
sahithi <sahithiduppati@gmail.com>
2022-07-29 12:52:20 +0530

    unfollow

--- backend-socialmedia-final/src/main/java/app/socialmedia/service/UserService.java
+++ backend-socialmedia-final/src/main/java/app/socialmedia/service/UserService.java
@@ -65,61 +65,47 @@ public class UserService {
     }
 
     public Response addFollower(int userId, int followerUserId){
+        Response response = new Response();
 
-        if(authService.loggedInUser != null) {
-            if(authService.loggedInUser.getUserId() == userId) {
-                Response response = new Response();
+        User user = userRepository.findById(userId);
 
-                User user = userRepository.findById(userId);
+        User follower = userRepository.findById(followerUserId);
 
-                User follower = userRepository.findById(followerUserId);
+        boolean find = false;
+        try{
+            if(authService.loggedInUser.getUserId() == user.getUserId()){
+                List<Integer> followersIds = user.getFollower();
+                List<Integer> followingIds = follower.getFollowing();
 
-                boolean find = false;
-                try {
-                    if (user != null && follower != null) {
-                        List<Integer> followersIds = user.getFollower();
-                        List<Integer> followingIds = follower.getFollowing();
-
-                        for (int Id : followersIds) {
-                            if (Id == followerUserId) {
-                                find = true;
-                                break;
-                            }
-                        }
-                        if (find) {
-                            response.setStatus(false);
-                            response.setMessage("Follower already exists");
-                            return response;
-                        } else {
-                            followersIds.add(followerUserId);
-                            followingIds.add(userId);
-                            user.setFollower(followersIds);
-                            follower.setFollowing(followingIds);
-                            userRepository.save(user);
-                            userRepository.save(follower);
-                            response.setStatus(true);
-                            response.setMessage("Follower added successfully");
-                        }
-                    } else if (user == null) {
-                        response.setStatus(false);
-                        response.setMessage("User doesn't exist");
-                    } else {
-                        response.setStatus(false);
-                        response.setMessage("Follower doesn't exist");
+                for(int Id : followersIds){
+                    if(Id == followerUserId){
+                        find = true;
+                        break;
                     }
-                    return response;
-                } catch (Exception e) {
-                    response.setStatus(false);
-                    response.setMessage("Follower could not be added");
-                    return response;
                 }
-            } else {
-                String message = "You are not authorized";
-                throw new NotAuthorizedException(message);
+                if(find){
+                    response.setStatus(false);
+                    response.setMessage("Follower already exists");
+                    return response;
+                }else{
+                    followersIds.add(followerUserId);
+                    followingIds.add(userId);
+                    user.setFollower(followersIds);
+                    follower.setFollowing(followingIds);
+                    userRepository.save(user);
+                    userRepository.save(follower);
+                    response.setStatus(true);
+                    response.setMessage("Follower added successfully");
+                }
+            }else{
+                response.setStatus(false);
+                response.setMessage("Error");
             }
-        } else {
-            String exceptionMessage = "You are not logged in.";
-            throw new NotLoggedInException(exceptionMessage);
+            return response;
+        }catch (Exception e){
+            response.setStatus(false);
+            response.setMessage("Follower could not be added");
+            return response;
         }
     }
 
@@ -130,7 +116,7 @@ public class UserService {
         Response response = new Response();
         Follower follower = new Follower();
 
-        if(user != null) {
+        if(authService.loggedInUser.getUserId() == user.getUserId()) {
             List<Integer> followersList = user.getFollower();
             if(followersList.isEmpty()){
                 response.setStatus(false);
@@ -158,15 +144,14 @@ public class UserService {
     }
 
     public Response removeFollower(int userId, int followerUserId){
-
-        if(authService.loggedInUser != null) {
-            if (authService.loggedInUser.getUserId() == userId) {
-                Response response = new Response();
-                User user = userRepository.findById(userId);
-                User follower = userRepository.findById(followerUserId);
+        Response response = new Response();
+        User user = userRepository.findById(userId);
+        User follower = userRepository.findById(followerUserId);
+        try{
+            if(authService.loggedInUser.getUserId() == user.getUserId()){
                 List<Integer> followersIds = user.getFollower();
                 List<Integer> followingIds = follower.getFollowing();
-                if (followersIds.contains(followerUserId) && followingIds.contains(userId)) {
+                if(followersIds.contains(followerUserId) && followingIds.contains(userId)){
                     followersIds.remove(followersIds.indexOf(followerUserId));
                     followingIds.remove(followingIds.indexOf(userId));
                 }
@@ -176,16 +161,50 @@ public class UserService {
                 userRepository.save(follower);
                 response.setStatus(true);
                 response.setMessage("Follower removed");
-
-                return response;
-            } else {
-                String message = "You are not authorized";
-                throw new NotAuthorizedException(message);
+            }else{
+                response.setStatus(false);
+                response.setMessage("Error");
             }
-        } else {
-            String exceptionMessage = "You are not logged in.";
-            throw new NotLoggedInException(exceptionMessage);
+            return response;
+
+        }catch (Exception e){
+            response.setStatus(false);
+            response.setMessage("Follower could not be removed");
+            return response;
         }
+
+    }
+
+    public Response unFollow(int userId, int followingUserId){
+        Response response = new Response();
+        User user = userRepository.findById(userId);
+        User following = userRepository.findById(followingUserId);
+        try{
+            if(authService.loggedInUser.getUserId() == user.getUserId()){
+                List<Integer> followingIds = user.getFollowing();
+                List<Integer> followerIds = following.getFollower();
+                if(followingIds.contains(followingUserId) && followerIds.contains(userId)){
+                    followingIds.remove(followingIds.indexOf(followingUserId));
+                    followerIds.remove(followerIds.indexOf(userId));
+                }
+                user.setFollowing(followingIds);
+                following.setFollower(followerIds);
+                userRepository.save(user);
+                userRepository.save(following);
+                response.setStatus(true);
+                response.setMessage("Unfollowed successfully");
+            }else{
+                response.setStatus(false);
+                response.setMessage("Error");
+            }
+            return response;
+
+        }catch (Exception e){
+            response.setStatus(false);
+            response.setMessage("Could not unfollow");
+            return response;
+        }
+
     }
 
 
@@ -200,20 +219,20 @@ public class UserService {
                 throw new UserNotFoundException(msg);
             }
 
-                if (authService.loggedInUser.getUserId() == userId) {
-                    try {
-                        userRepository.delete(deleteUser);
-                        deleteUserResponse.setStatus(true);
-                        deleteUserResponse.setMessage("Deleted Successfully");
-                    }
-                    catch (Exception e){
-                        String msg= "The user cannot be deleted";
-                        throw new ActionCannotBeCompletedException(msg);
-                    }
-                } else {
-                    String msg = "You cant delete someone else's profile";
-                    throw new NotAuthorizedException(msg);
+            if (authService.loggedInUser.getUserId() == userId) {
+                try {
+                    userRepository.delete(deleteUser);
+                    deleteUserResponse.setStatus(true);
+                    deleteUserResponse.setMessage("Deleted Successfully");
                 }
+                catch (Exception e){
+                    String msg= "The user cannot be deleted";
+                    throw new ActionCannotBeCompletedException(msg);
+                }
+            } else {
+                String msg = "You cant delete someone else's profile";
+                throw new NotAuthorizedException(msg);
+            }
 
 
             return deleteUserResponse;
@@ -225,30 +244,30 @@ public class UserService {
     }
 
     public Response viewPublicProfile(String email){
-            Response searchUserResponse= new Response();
-            try{
-                User user = userRepository.findByEmail(email);
-                PublicProfile publicProfile= new PublicProfile();
-                publicProfile.setFullName(user.getFullName());
-                publicProfile.setUserName(user.getUserName());
-                publicProfile.setProfilePicUrl(user.getProfilePicUrl());
-                publicProfile.setNoOfFollowers(user.getFollower().size());
-                publicProfile.setNoOfFollowing(user.getFollowing().size());
-                searchUserResponse.setStatus(true);
-                searchUserResponse.setMessage("Found successfully");
-                searchUserResponse.setPayload(publicProfile);
-            }
-            catch(NullPointerException e){
-                String msg= "User with emailId "+email+" does not exist";
-                throw new UserNotFoundException(msg);
-            }
-            catch(Exception e){
-                String msg = "Action cannot be completed";
-
-                throw new ActionCannotBeCompletedException(msg);
-            }
-            return searchUserResponse;
+        Response searchUserResponse= new Response();
+        try{
+            User user = userRepository.findByEmail(email);
+            PublicProfile publicProfile= new PublicProfile();
+            publicProfile.setFullName(user.getFullName());
+            publicProfile.setUserName(user.getUserName());
+            publicProfile.setProfilePicUrl(user.getProfilePicUrl());
+            publicProfile.setNoOfFollowers(user.getFollower().size());
+            publicProfile.setNoOfFollowing(user.getFollowing().size());
+            searchUserResponse.setStatus(true);
+            searchUserResponse.setMessage("Found successfully");
+            searchUserResponse.setPayload(publicProfile);
         }
+        catch(NullPointerException e){
+            String msg= "User with emailId "+email+" does not exist";
+            throw new UserNotFoundException(msg);
+        }
+        catch(Exception e){
+            String msg = "Action cannot be completed";
+
+            throw new ActionCannotBeCompletedException(msg);
+        }
+        return searchUserResponse;
+    }
 
     public ViewProfile getProfile(String email){
         if (authService.loggedInUser != null) {
@@ -274,8 +293,8 @@ public class UserService {
         else{
             String exceptionMessage = "You are not logged in.";
             throw new NotLoggedInException(exceptionMessage);
-            }
         }
     }
+}
 
 
