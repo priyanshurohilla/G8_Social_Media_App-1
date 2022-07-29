a4b8637dafc4156acd9583236f29c9e38b680aeb
Priyanshu Rohilla <priyanshurohillaverma@gmail.com>
2022-07-29 14:30:47 +0530

    View My posts functionality added

--- backend-socialmedia-final/src/main/java/app/socialmedia/service/PostService.java
+++ backend-socialmedia-final/src/main/java/app/socialmedia/service/PostService.java
@@ -44,8 +44,11 @@ public class PostService {
 
                 try {
                     int postId = mongoDbSequenceGeneratorService.getNextSequence(Post.POST_SEQUENCE_NAME);
+                    post.setCreatedByUserId(authService.loggedInUser.getUserId());
                     post.setPostId(postId);
                     post.setTimestamp(Instant.now());
+                    System.out.println(post.getImageUrl());
+                    post.setImageUrl(post.getImageUrl());
                     postRepository.save(post);
                     response.setStatus(true);
                     response.setPayload(post);
@@ -98,106 +101,6 @@ public class PostService {
 
     }
 
-  public Response editPost(Post post) {
-    Response response = new Response();
-    if (authService.loggedInUser != null) {
-
-      if (post.getContent().equals("") && post.getImageUrl().equals("")) {
-        response = new Response();
-        response.setStatus(false);
-        response.setMessage("Empty Post");
-        return response;
-      }
-      try {
-        Post postToBeEdited = postRepository.findById(post.getPostId());
-        postToBeEdited.setContent(post.getContent());
-        postToBeEdited.setImageUrl(post.getImageUrl());
-        postRepository.save(postToBeEdited);
-        response.setStatus(true);
-        response.setMessage("Post edited successfully : " + post.getPostId());
-      } catch (Exception e) {
-        response.setStatus(false);
-        response.setMessage("Post could not be edited");
-      }
-      return response;
-    } else {
-      String exceptionMessage = "You are not logged in.";
-      throw new NotLoggedInException(exceptionMessage);
-    }
-  }
-
-  public Response addComment(CommentRequest commentRequest) {
-    Response response = new Response();
-
-    if (authService.loggedInUser != null) {
-
-      try {
-
-        User user = userRepository.findById(commentRequest.getUserId());
-        Post post = postRepository.findById(commentRequest.getPostId());
-
-        if (commentRequest.getContent().equals("")) {
-          response = new Response();
-          response.setStatus(false);
-          response.setMessage("content should not be blank!");
-          return response;
-        } else {
-          CommentEntity newComment = new CommentEntity();
-          String commentId = UUID.randomUUID().toString();
-          newComment.setCommentId(commentId);
-          newComment.setPostId(post.getPostId());
-          newComment.setUserId(commentRequest.getUserId());
-          newComment.setFullName(user.getFullName());
-          newComment.setProfileUrl(user.getProfilePicUrl());
-          newComment.setContent(commentRequest.getContent());
-          newComment.setTimeStamp(Instant.now());
-
-          List<String> commentIdList = post.getComments();
-          commentIdList.add(commentId);
-          post.setComments(commentIdList);
-
-          commentRepository.save(newComment);
-          postRepository.save(post);
-
-          response.setStatus(true);
-          response.setMessage("Created comment and comment Id:" + newComment.getCommentId());
-          response.setPayload(newComment);
-        }
-      } catch (Exception e) {
-        response.setStatus(false);
-        response.setMessage("Could not create comment, invalid user id or post Id");
-      }
-      return response;
-    } else {
-      String exceptionMessage = "You are not logged in.";
-      throw new NotLoggedInException(exceptionMessage);
-    }
-  }
-
-  public Response deleteComment(String commentId) {
-    Response response = new Response();
-    if (authService.loggedInUser != null) {
-      try {
-        CommentEntity commentEntity = commentRepository.findByCommentId(commentId);
-        Post post = postRepository.findById(commentEntity.getPostId());
-        List<String> commentList = post.getComments();
-        commentList.remove(commentId);
-        post.setComments(commentList);
-        postRepository.save(post);
-        commentRepository.delete(commentEntity);
-        response.setStatus(true);
-        response.setMessage(" Comment successfully deleted");
-      } catch (Exception e) {
-        String message = "comment not found with commentId " + commentId;
-        throw new PostNotFoundException(message);
-      }
-
-    } else {
-      String exceptionMessage = "You are not logged in.";
-      throw new NotLoggedInException(exceptionMessage);
-    }
-    return response;
-  }
 
   public Response ViewAllComment(int postId)
   {   Response response = new Response();
@@ -237,11 +140,12 @@ public Response viewMyPosts(int userId){
               String msg = "No posts found";
               throw new PostNotFoundException(msg);
           }
-          ViewMyPostsEntity viewMyPostsEntity = new ViewMyPostsEntity();
+
           List<ViewMyPostsEntity> viewMyPosts = new ArrayList<>();
           if(authService.loggedInUser.getUserId()==userId){
             try {
                 for (Post post : posts) {
+                    ViewMyPostsEntity viewMyPostsEntity = new ViewMyPostsEntity();
                     viewMyPostsEntity.setPostId(post.getPostId());
                     viewMyPostsEntity.setContent(post.getContent());
                     viewMyPostsEntity.setNumberOfComments(post.getComments().size());
@@ -424,7 +328,7 @@ public Response viewMyPosts(int userId){
     public List<Post> getPosts(List<Integer> postIds) {
         List<Post> responsePosts = new ArrayList<>();
         for (int postId : postIds) {
-            Post post = postRepository.findByPostId(postId);
+            Post post = postRepository.findById(postId);
             if (post != null) {
                 responsePosts.add(post);
             }
@@ -438,7 +342,7 @@ public Response viewMyPosts(int userId){
         if(authService.loggedInUser != null) {
             if (authService.loggedInUser.getUserId() == likeRequest.getUserId()) {
                 Response response = new Response();
-                Post post = postRepository.findByPostId(likeRequest.getPostId());
+                Post post = postRepository.findById(likeRequest.getPostId());
                 response = new Response();
                 if (post.getLikedBy().contains(likeRequest.getUserId())) {
                     removeLike(post, likeRequest.getUserId());
@@ -471,7 +375,7 @@ public Response viewMyPosts(int userId){
         if(authService.loggedInUser != null){
             Response response = new Response();
             response = new Response();
-            Post post = postRepository.findByPostId(postId);
+            Post post = postRepository.findById(postId);
             if (post == null) {
                 response.setStatus(false);
                 response.setMessage("Post does not exist!!");
